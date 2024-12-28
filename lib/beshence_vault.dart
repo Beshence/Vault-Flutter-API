3e59b8ad8794290b56854b401891ba991b18d274
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2024-12-28 18:43:13 +0300

    now can get vault info

--- lib/beshence_vault.dart
+++ lib/beshence_vault.dart
@@ -1,5 +1,47 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+import 'package:universal_io/io.dart';
+
+class BeshenceVaultInfo {
+  final String hello;
+  final String apiVersion;
+
+  BeshenceVaultInfo({required this.hello, required this.apiVersion});
+}
+
+class BeshenceVault {
+  final String address;
+  final String? token;
+  late final Dio dio;
+
+  BeshenceVault({required this.address, this.token}) {
+    dio = _getDio(token: token);
+  }
+
+  Future<BeshenceVaultInfo> getVaultInfo() async {
+    final serverHelloResponse = (await dio.get('https://$address/api/hello'));
+    print(serverHelloResponse.data);
+    return BeshenceVaultInfo(
+        hello: serverHelloResponse.data["response"]["hello"],
+        apiVersion: serverHelloResponse.data["response"]["api_version"]
+    );
+  }
+}
+
+Dio _getDio({String? token}) {
+  final dio = Dio();
+
+  (dio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = () {
+    final client = HttpClient();
+    // TODO: change it
+    client.badCertificateCallback = (cert, host, port) => true;
+    return client;
+  };
+
+  dio.options.headers = {
+    if (token != null) "Authorization": "Bearer $token"
+  };
+  dio.options.validateStatus = (code) => true;
+
+  return dio;
 }
